e7f834cb0bd1b6fea25548122b7978e58d26ac44
azrafe7 <azrafe7@gmail.com>
2016-11-28 16:40:04 +0100

    Update MyApp code & metadata to 1.0.1

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Squirrel;
 
 namespace MyApp
 {
@@ -17,6 +18,16 @@ namespace MyApp
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            updateApp();
         }
+
+        async static Task updateApp() {
+            using (var mgr = new UpdateManager(@"d:\Downloads\Dev\Haxe\MyApp.Squirrel_git\Releases\"))
+            {
+                await mgr.UpdateApp();
+            }
+        }
+
     }
 }
